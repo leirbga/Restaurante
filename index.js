520fba79c66df50d1b407915a2fc9eb8d7e2da08
leirbga <smortad.legedary@gmail.com>
2026-06-21 10:19:00 -0400

    correciones

--- index.js
+++ index.js
@@ -4,66 +4,74 @@ const pizza = document.querySelector('#estatusPizza');
 const bebida = document.querySelector('#estatusBebida');
 const postre = document.querySelector('#estatusPostre');
 
-btnEnviarPedido.addEventListener('click',(evento) =>{
-    estatusPedido.innerHTML = `<p>Procesando Pedido...</p>`;
-    preparacionPedido();
-});
-
 const estatusDelPedido = () =>{
-    const pedido = Math.random() < 0.8;
-    return pedido;
+    return Math.random() < 0.8;
 };
 
-const pedidoPizza = () =>{
+const pedidoPizza = (pedidoExitoso) =>{
     return new Promise((resolve, reject)=>{
         setTimeout(() => {
-            if(estatusDelPedido()){
+            if(pedidoExitoso){
                 resolve('Pizza Lista!')
                 pizza.innerHTML = `Pizza Lista ✅`;
             }else{
-                reject('Ocurrio un error...')
-                pizza.innerHTML = `Error ❌`;
+                reject('Error en la Pizza ❌');
             }
         }, 3000);
     })
 };
 
-const pedidoBebida = () =>{
+const pedidoBebida = (pedidoExitoso) =>{
      return new Promise((resolve, reject)=>{
         setTimeout(() => {
-            if(estatusDelPedido()){
+            if(pedidoExitoso){
                 resolve('Bebida Lista!')
                 bebida.innerHTML = `Bebida Lista ✅`;
             }else{
-                reject('Ocurrio un error...')
-                bebida.innerHTML = `Error ❌`;
+                reject('Error en la Bebida ❌');
             }
         }, 3000);
     })
 };
 
-const pedidoPostre = () =>{
+const pedidoPostre = (pedidoExitoso) =>{
  return new Promise((resolve, reject)=>{
         setTimeout(() => {
-            if(estatusDelPedido()){
+            if(pedidoExitoso){
                 resolve('Postre Listo')
                 postre.innerHTML = `Postre Listo ✅`;
             }else{
-                reject('Ocurrio un error...')
-                postre.innerHTML = `Error ❌`;
+                reject('Error en la Bebida ❌');
             }
         }, 3000);
     })
 };
 
 
-const preparacionPedido = async () =>{
+const preparacionPedido = async (pedidoExitoso) =>{
     try{
-       const pizzaLista = await pedidoPizza();
-       const bebidaLista = await pedidoBebida();
-       const postreListo = await pedidoPostre();
-       estatusPedido.innerHTML = `<p>Tu pedido ha sido procesado con exito ✅</p>`;
+        const pizzaLista = await pedidoPizza(pedidoExitoso);
+        const bebidaLista = await pedidoBebida(pedidoExitoso);
+        const postreListo = await pedidoPostre(pedidoExitoso);
     }catch(error){
-        estatusPedido.innerHTML = `<p>Ocurrio un error con tu pedido, intente nuevamente</p>`;
+        console.log(error);
     }
 };
+
+
+btnEnviarPedido.addEventListener('click',(evento) =>{
+    estatusPedido.innerHTML = `<p>Procesando Pedido...</p>`;
+    pizza.innerHTML = ``;
+    bebida.innerHTML = ``;
+    postre.innerHTML = ``;
+
+    const pedidoAprobado = estatusDelPedido();
+
+    if(pedidoAprobado){
+        estatusPedido.innerHTML = `<p>Tu pedido ha sido procesado con exito ✅</p>`;
+        preparacionPedido(pedidoAprobado);
+    }else{
+        estatusPedido.innerHTML = `<p>Ocurrio un error con tu pedido, intente nuevamente</p>`;
+    }
+    
+});
